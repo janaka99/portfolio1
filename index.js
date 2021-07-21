8dcc069e5be1edec9fced9cb6a549b7636354598
janaka <janakachamith882gmail.com>
2021-07-21 20:17:04 +0530

    smsgateaway

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose')
 
 const app = express();
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/portfolio';
+
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -16,11 +32,17 @@ app.get('/', (req, res)=>{
 
 app.get('/web-develop-projects', (req, res)=>{
     res.render('dynamic/webdev')
-})
+});
 
 app.get('/app-develop-projects', (req, res)=>{
     res.render('dynamic/webdev')
-})
+});
+ 
+app.get('/seo-projects', (req, res)=>{
+    res.render('dynamic/webdev')
+});
+
+
 
 
 app.listen(process.env.PORT || 5000);
